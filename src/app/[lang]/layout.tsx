047f6c063ad2a1651fb66d5ca6a357e0aec6670b
Loftal <info@loftal.jp>
2025-05-15 08:50:31 +0630

    feat(add theme feature): add theme, created nav-link for localized route

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from 'next';
+import type { Metadata, Viewport } from 'next';
 import { GeistSans } from 'geist/font/sans';
 
 import '../globals.css';
@@ -6,6 +6,15 @@ import '../globals.css';
 import { ThemeProvider } from '@/components/providers/theme-provider';
 import NextTopLoader from 'nextjs-toploader';
 import { Locale } from '@/features/internationalization/i18n-config';
+import Providers from '@/components/providers/providers';
+import { cookies } from 'next/headers';
+import { fontVariables } from '@/lib/font';
+import { cn } from '@/lib/utils';
+
+const META_THEME_COLORS = {
+  light: '#ffffff',
+  dark: '#09090b'
+};
 
 export const metadata: Metadata = {
   metadataBase: new URL(
@@ -15,24 +24,24 @@ export const metadata: Metadata = {
         ? `https://${process.env.VERCEL_URL}`
         : `http://localhost:${process.env.PORT || 3000}`
   ),
-  title: 'shadcn/ui sidebar',
+  title: 'SmartGrade UCSH',
   description:
-    'A stunning and functional retractable sidebar for Next.js built on top of shadcn/ui complete with desktop and mobile responsiveness.',
+    'Simplifying Student Grading & Evaluation System at University of Computer Studies, Hinthada',
   alternates: {
     canonical: '/'
   },
   openGraph: {
     url: '/',
-    title: 'shadcn/ui sidebar',
+    title: 'SmartGrade UCSH',
     description:
-      'A stunning and functional retractable sidebar for Next.js built on top of shadcn/ui complete with desktop and mobile responsiveness.',
+      'Simplifying Student Grading & Evaluation System at University of Computer Studies, Hinthada',
     type: 'website'
   },
   twitter: {
     card: 'summary_large_image',
-    title: 'shadcn/ui sidebar',
+    title: 'SmartGrade UCSH',
     description:
-      'A stunning and functional retractable sidebar for Next.js built on top of shadcn/ui complete with desktop and mobile responsiveness.'
+      'Simplifying Student Grading & Evaluation System at University of Computer Studies, Hinthada'
   }
 };
 
@@ -40,19 +49,35 @@ export async function generateStaticParams() {
   return [{ lang: 'en' }, { lang: 'mm' }];
 }
 
+export const viewport: Viewport = {
+  themeColor: META_THEME_COLORS.light
+};
+
 export default async function RootLayout(props: {
   children: React.ReactNode;
   params: Promise<{ lang: Locale }>;
 }) {
   const params = await props.params;
   const { children } = props;
+  const cookieStore = await cookies();
+  const activeThemeValue = cookieStore.get('active_theme')?.value;
+  const isScaled = activeThemeValue?.endsWith('-scaled');
 
   return (
     <html lang={params.lang} suppressHydrationWarning>
-      <body className={GeistSans.className}>
+      <body
+        className={cn(
+          'bg-background overflow-hidden overscroll-none font-sans antialiased',
+          activeThemeValue ? `theme-${activeThemeValue}` : '',
+          isScaled ? 'theme-scaled' : '',
+          fontVariables
+        )}
+      >
+        <NextTopLoader showSpinner={false} />
         <ThemeProvider attribute='class' defaultTheme='system' enableSystem>
-          <NextTopLoader />
-          {children}
+          <Providers activeThemeValue={activeThemeValue as string}>
+            {children}
+          </Providers>
         </ThemeProvider>
       </body>
     </html>
