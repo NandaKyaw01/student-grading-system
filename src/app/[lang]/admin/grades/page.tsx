af209fb8b7e34aa0edd1914f843db458de4a88b9
LastAppartment305 <rapskill095@gmail.com>
2025-05-25 17:16:56 +0630

    refactor: merge conflict
    resolve merge conflict after rebase

--- src/app/[lang]/admin/grades/page.tsx
+++ src/app/[lang]/admin/grades/page.tsx
@@ -1,3 +1,4 @@
+'use client';
 import Link from 'next/link';
 
 import PlaceholderContent from '@/components/demo/placeholder-content';
---
+++ src/components/breadcrumb-list.ts
@@ -0,0 +1,14 @@
+const BreadcrumbList = [
+  {
+    title: 'Home',
+    link: '/'
+  },
+  {
+    title: 'Dashboard',
+    Link: '/admin/dashboard'
+  },
+  {
+    title: 'Categories',
+    Link: '/admin/categories'
+  }
+];
