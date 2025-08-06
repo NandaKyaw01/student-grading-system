59ca009dbfda3684aa589ec432772dd29f4acaea
Nanda Kyaw <nandakyaw.dev@gmail.com>
2025-08-06 20:06:25 +0630

    fix ts error on route

--- src/middleware.ts
+++ src/middleware.ts
@@ -13,48 +13,59 @@ const publicPages = [
 
 const intlMiddleware = createMiddleware(routing);
 
-const authMiddleware = withAuth(async (req) => {
+const authMiddleware = withAuth(
+  async (req) => {
     const response = intlMiddleware(req);
 
     const token = req.nextauth.token;
-    
+
     if (token) {
-      
-      const apiUrl = new URL(`/api/validate-user/${token.id}`, req.nextUrl.origin)
+      const apiUrl = new URL(
+        `/api/validate-user/${token.id}`,
+        req.nextUrl.origin
+      );
 
       const userResponse = await fetch(apiUrl, {
         headers: {
-          'Content-Type': 'application/json',
+          'Content-Type': 'application/json'
         }
-      })
+      });
 
       if (!userResponse.ok) {
-        throw new Error('Failed to fetch user')
+        throw new Error('Failed to fetch user');
       }
 
-      const user = await userResponse.json()
-      
-      if (!user || (token.updatedAt && new Date(token.updatedAt) < new Date(user.updatedAt))) {
+      const user = await userResponse.json();
+
+      if (
+        user &&
+        token.updatedAt &&
+        user.updatedAt &&
+        new Date(token.updatedAt) < new Date(user.updatedAt)
+      ) {
         const url = new URL('/auth/login', req.url);
         url.searchParams.set('callbackUrl', req.nextUrl.pathname);
-        const cookiePrefix = process.env.NODE_ENV === 'production' ? '__Secure-' : ''
-        response.cookies.delete(`${cookiePrefix}next-auth.session-token`)
-        response.cookies.delete(`${cookiePrefix}next-auth.callback-url`)
-        response.cookies.delete(`${cookiePrefix}next-auth.csrf-token`)
-        response.cookies.delete(`__Host-next-auth.csrf-token`)
+        const cookiePrefix =
+          process.env.NODE_ENV === 'production' ? '__Secure-' : '';
+        response.cookies.delete(`${cookiePrefix}next-auth.session-token`);
+        response.cookies.delete(`${cookiePrefix}next-auth.callback-url`);
+        response.cookies.delete(`${cookiePrefix}next-auth.csrf-token`);
+        response.cookies.delete(`__Host-next-auth.csrf-token`);
         return NextResponse.redirect(url);
       }
     }
 
     return response;
-}, {
-  callbacks: {
-    authorized: ({ token }) => token != null
   },
-  pages: {
-    signIn: '/auth/login'
+  {
+    callbacks: {
+      authorized: ({ token }) => token != null
+    },
+    pages: {
+      signIn: '/auth/login'
+    }
   }
-});
+);
 
 export default function middleware(req: NextRequest) {
   const publicPathnameRegex = RegExp(
